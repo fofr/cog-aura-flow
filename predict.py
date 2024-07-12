4f468019320025053c77683297c3a888f36e1aea
Paul <fofr@users.noreply.github.com>
2024-07-12 09:11:34 +0000

    Add more settings

--- predict.py
+++ predict.py
@@ -28,30 +28,25 @@ class Predictor(BasePredictor):
         self.comfyUI = ComfyUI("127.0.0.1:8188")
         self.comfyUI.start_server(OUTPUT_DIR, INPUT_DIR)
 
-        # Give a list of weights filenames to download during setup
         with open(api_json_file, "r") as file:
             workflow = json.loads(file.read())
         self.comfyUI.handle_weights(
             workflow,
-            weights_to_download=[],
+            weights_to_download=[
+                "aura_flow_0.1.safetensors",
+            ],
         )
-
-    def filename_with_extension(self, input_file, prefix):
-        extension = os.path.splitext(input_file.name)[1]
-        return f"{prefix}{extension}"
-
-    def handle_input_file(
-        self,
-        input_file: Path,
-        filename: str = "image.png",
-    ):
-        shutil.copy(input_file, os.path.join(INPUT_DIR, filename))
 
     def update_workflow(self, workflow, **kwargs):
         sampler = workflow["3"]["inputs"]
         sampler["seed"] = kwargs["seed"]
         sampler["sampler"] = kwargs["sampler"]
         sampler["scheduler"] = kwargs["scheduler"]
+
+        empty_latent_image = workflow["5"]["inputs"]
+        empty_latent_image["width"] = kwargs["width"]
+        empty_latent_image["height"] = kwargs["height"]
+        empty_latent_image["batch_size"] = kwargs["number_of_images"]
 
         shift = workflow["10"]["inputs"]
         shift["shift"] = kwargs["shift"]
@@ -62,10 +57,6 @@ class Predictor(BasePredictor):
         negative_prompt = workflow["7"]["inputs"]
         negative_prompt["text"] = f"nsfw, {kwargs['negative_prompt']}"
 
-        if kwargs["image_filename"]:
-            # workflow["1"]["inputs"]["image"] = kwargs["image_filename"]
-            pass
-
     def predict(
         self,
         prompt: str = Input(
@@ -74,10 +65,6 @@ class Predictor(BasePredictor):
         negative_prompt: str = Input(
             description="Things you do not want to see in your image",
             default="",
-        ),
-        image: Path = Input(
-            description="An input image",
-            default=None,
         ),
         width: int = Input(
             description="The width of the image",
@@ -131,11 +118,6 @@ class Predictor(BasePredictor):
         self.comfyUI.cleanup(ALL_DIRECTORIES)
         seed = seed_helper.generate(seed)
 
-        image_filename = None
-        if image:
-            image_filename = self.filename_with_extension(image, "image")
-            self.handle_input_file(image_filename)
-
         with open(api_json_file, "r") as file:
             workflow = json.loads(file.read())
 
@@ -143,13 +125,15 @@ class Predictor(BasePredictor):
             workflow,
             prompt=prompt,
             negative_prompt=negative_prompt,
-            image_filename=image_filename,
             seed=seed,
             steps=steps,
             cfg=cfg,
             sampler=sampler,
             scheduler=scheduler,
             shift=shift,
+            width=width,
+            height=height,
+            number_of_images=number_of_images,
         )
 
         self.comfyUI.connect()
